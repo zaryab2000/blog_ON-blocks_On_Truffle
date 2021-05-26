078999f4b23e9fd49ccb25797acfcb5c48978bd8
zar2000 <cryptoanalysis64@gmail.com>
2021-05-26 10:46:38 +0530

    added Migration and Test Cases

--- migrations/2_bob.js
+++ migrations/2_bob.js
@@ -1,5 +1,6 @@
 const BobContract = artifacts.require("BlogOnBlocks");
-
+// Redisigning Migrations
 module.exports = function(deployer) {
+  console.log("DEPLOYMENT STARTED")
   deployer.deploy(BobContract);
 };
